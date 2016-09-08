5aac469415678680d5629f4ba4abc03b3d5a1edb
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-09-08 16:58:40 -0300

    feat: Add width and height properties to Enemy and Player

--- js/app.js
+++ js/app.js
@@ -5,8 +5,12 @@ var enemyPositions = [50, 143, 225];
 var Enemy = function(speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 0;
-    this.y = enemyPositions[getRandomIntInclusive(0, enemyPositions.length-1)];
+    this.spriteX = 0;
+    this.spriteY = enemyPositions[getRandomIntInclusive(0, enemyPositions.length-1)];
+    this.x = this.spriteX+2;
+    this.y = this.spriteY+78;
+    this.width = 93;
+    this.height = 66;
     this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -19,8 +23,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x + (this.speed * dt) <= xBounds.max) {
-        this.x += this.speed * dt;
+    if (this.spriteX + (this.speed * dt) <= xBounds.max) {
+        this.spriteX += this.speed * dt;
+        this.x = this.spriteX+2;
+        this.y = this.spriteY+78;
     } else {
         this.reset(); // Reset enemy position
     }
@@ -28,42 +34,70 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // ctx.clearRect(this.spriteX+2, this.spriteY+78, 93, 66);
+    ctx.drawImage(Resources.get(this.sprite), this.spriteX, this.spriteY);
+    // ctx.rect(this.spriteX+2, this.spriteY+78, 93, 66);
+    // ctx.stroke();
 };
 Enemy.prototype.reset = function () {
-    this.x = 0;
-    this.y = enemyPositions[getRandomIntInclusive(0, enemyPositions.length-1)];
-}
+    this.spriteX = 0;
+    this.spriteY = enemyPositions[getRandomIntInclusive(0, enemyPositions.length-1)];
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function () {
-    this.x = 2*101;
-    this.y = 383;
+    this.spriteX = 2*101;
+    this.spriteY = 383;
+    this.x = this.spriteX+17;
+    this.y = this.spriteY+63;
+    this.width = 68;
+    this.height = 77;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function (input) {
     if (input === "left") {
-        (this.x - 101) >= xBounds.min ? this.x -= 101 : this.x -= 0;
+        // (this.spriteX - 101) >= xBounds.min ? this.spriteX -= 101 : this.spriteX -= 0;
+        if ((this.spriteX - 101) >= xBounds.min) {
+            this.spriteX -= 101;
+            this.x = this.spriteX+17;
+        }
     } else if (input === "right") {
-        (this.x + 101) < xBounds.max ? this.x += 101 : this.x += 0;
+        // (this.spriteX + 101) < xBounds.max ? this.spriteX += 101 : this.spriteX += 0;
+        if ((this.spriteX + 101) < xBounds.max) {
+            this.spriteX += 101;
+            this.x = this.spriteX+17;
+        }
     } else if (input === "up") {
-        (this.y - 83) > yBounds.min ? this.y -= 83 : this.reset();
+        // (this.spriteY - 83) > yBounds.min ? this.spriteY -= 83 : this.reset();
+        if ((this.spriteY - 83) > yBounds.min) {
+            this.spriteY -= 83;
+            this.y = this.spriteY+63;
+        } else {
+            this.reset();
+        }
     } else if (input === "down") {
-        (this.y + 83) <= yBounds.max ? this.y += 83 : this.x += 0;
+        // (this.spriteY + 83) <= yBounds.max ? this.spriteY += 83 : this.spriteX += 0;
+        if ((this.spriteY + 83) <= yBounds.max) {
+            this.spriteY += 83;
+            this.y = this.spriteY+63;
+        }
     }
 };
 Player.prototype.handleInput = function (input) {
     this.update(input);
 };
 Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // ctx.clearRect(this.spriteX+17, this.spriteY+63, 68, 77);
+    ctx.drawImage(Resources.get(this.sprite), this.spriteX, this.spriteY);
+    // ctx.rect(this.spriteX+17, this.spriteY+63, 68, 77);
+    // ctx.stroke();
 };
 Player.prototype.reset = function () {
-    this.x = 2*101;
-    this.y = 383;
+    this.spriteX = 2*101;
+    this.spriteY = 383;
 };
 
 // Returns a random integer between min (included) and max (included)
